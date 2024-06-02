af229fc2b0c2b38949dca53b10ffd92ae15510e1
violetochollah <vioachieng90@gmail.com>
2024-06-02 17:54:48 +0300

    updates

---
+++ Backend/cmd/server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"whatsapp-clone/pkg/handlers"
+	"whatsapp-clone/pkg/websocket"
+)
+
+func main() {
+	go websocket.HubInstance.Run()
+	http.HandleFunc("/", handlers.ServeHome)
+	http.HandleFunc("/ws", handlers.ServeWs)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ whatsapp-clone-frontend/src/App.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect, useRef } from 'react';
+import './/App.css';
+ 
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+  const ws = useRef(null);
+
+  useEffect(() => {
+    ws.current = new WebSocket("ws://localhost:8080/ws");
+    ws.current.onmessage = (event) => {
+      setMessages(prevMessages => [...prevMessages, event.data]);
+    };
+    return () => {
+      ws.current.close();
+    };
+  },[]);
+
+  const sendMessage = () => {
+    ws.current.send(input);
+    setInput('');
+  };
+  return (
+    <div className="app">
+      <div className="chat_window">
+        <ul className="message">
+          {messages.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+          </ul>
+          <div className="input-container">
+            <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message..."
+            />
+            <button onclick={sendMessage}>send</button>
+            </div>
+            </div>
+            </div>
+
+        );
+      }
+
+      export default App;
